078db20a7baed353926b424918eb577249e6c235
godc <caiyenan99@163.com>
2023-07-04 04:13:12 +0800

    Channel模块功能基本完成

--- src/api/tienchin/channel.js
+++ src/api/tienchin/channel.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 
-// 查询角色列表
+// 查询渠道列表
 export function listChannel(query) {
     return request({
         url: '/tienchin/channel/list',
@@ -9,15 +9,15 @@ export function listChannel(query) {
     })
 }
 
-// 查询角色详细
-export function getRole(roleId) {
+// 查询渠道详细
+export function getChannel(roleId) {
     return request({
-        url: '/system/role/' + roleId,
+        url: '/tienchin/channel/' + roleId,
         method: 'get'
     })
 }
 
-// 新增角色
+// 新增渠道
 export function addChannel(data) {
     return request({
         url: '/tienchin/channel',
@@ -26,94 +26,22 @@ export function addChannel(data) {
     })
 }
 
-// 修改角色
-export function updateRole(data) {
+// 修改渠道
+export function updateChannel(data) {
     return request({
-        url: '/system/role',
+        url: '/tienchin/channel',
         method: 'put',
         data: data
     })
 }
 
-// 角色数据权限
-export function dataScope(data) {
-    return request({
-        url: '/system/role/dataScope',
-        method: 'put',
-        data: data
-    })
-}
 
-// 角色状态修改
-export function changeRoleStatus(roleId, status) {
-    const data = {
-        roleId,
-        status
-    }
-    return request({
-        url: '/system/role/changeStatus',
-        method: 'put',
-        data: data
-    })
-}
 
 // 删除角色
-export function delRole(roleId) {
+export function deleteChannel(channel) {
     return request({
-        url: '/system/role/' + roleId,
+        url: '/tienchin/channel/' + channel,
         method: 'delete'
     })
 }
 
-// 查询角色已授权用户列表
-export function allocatedUserList(query) {
-    return request({
-        url: '/system/role/authUser/allocatedList',
-        method: 'get',
-        params: query
-    })
-}
-
-// 查询角色未授权用户列表
-export function unallocatedUserList(query) {
-    return request({
-        url: '/system/role/authUser/unallocatedList',
-        method: 'get',
-        params: query
-    })
-}
-
-// 取消用户授权角色
-export function authUserCancel(data) {
-    return request({
-        url: '/system/role/authUser/cancel',
-        method: 'put',
-        data: data
-    })
-}
-
-// 批量取消用户授权角色
-export function authUserCancelAll(data) {
-    return request({
-        url: '/system/role/authUser/cancelAll',
-        method: 'put',
-        params: data
-    })
-}
-
-// 授权用户选择
-export function authUserSelectAll(data) {
-    return request({
-        url: '/system/role/authUser/selectAll',
-        method: 'put',
-        params: data
-    })
-}
-
-// 根据角色ID查询部门树结构
-export function deptTreeSelect(roleId) {
-    return request({
-        url: '/system/role/deptTree/' + roleId,
-        method: 'get'
-    })
-}
